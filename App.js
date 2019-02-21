65d7592967c9529e3480db7bfbb424d858e7be50
JeffAntoine <Janto031@fiu.edu>
2019-02-20 23:43:45 -0500

    basic app

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import InstaClone from "./src/components/InstaClone";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <InstaClone/>
       </View>
     );
   }
---
+++ src/components/InstaClone.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, image, Dimensions} from 'react-native'
+
+class InstaClone extends Component{
+    render() {
+        return(
+            <View>
+
+            </View>
+        );
+    }
+}
+
+
+export default InstaClone;
